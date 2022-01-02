88468ec5557e1329c653422a2d4c36b9c947214d
pkarthikeyan1312 <96464279+pkarthikeyan1312@users.noreply.github.com>
2022-01-02 21:02:16 +0530

    Hello Devops

---
+++ DevopsAssignmentApplication.java
@@ -0,0 +1,13 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
