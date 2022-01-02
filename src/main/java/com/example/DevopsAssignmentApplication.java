b61dcbb1b3f00c464f183089e00820cbcf4c28ee
pkarthikeyan1312 <pkarthike@hcl.com>
2022-01-02 20:50:08 +0530

    Hello Devops

--- src/main/java/com/example/DevopsAssignmentApplication.java
+++ src/main/java/com/example/DevopsAssignmentApplication.java
@@ -8,7 +8,7 @@ public class DevopsAssignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsAssignmentApplication.class, args);
-		System.out.println("Hello Devops");
+		System.out.println("Hello Dev-Ops");
 	}
 
 }
