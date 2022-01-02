d89a8503f63f74ec8b24f7b6ee57155c1b6ed645
pkarthikeyan1312 <96464279+pkarthikeyan1312@users.noreply.github.com>
2022-01-02 21:32:06 +0530

    Hello Devops

--- src/main/java/com/example/DevopsAssignmentApplication.java
+++ src/main/java/com/example/DevopsAssignmentApplication.java
@@ -1,14 +1,20 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class DevopsAssignmentApplication {
+public class DevopsAssignmentApplication implements CommandLineRunner {
+	
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsAssignmentApplication.class, args);
-		System.out.println("Hello Dev-Ops");
+		public static void main(String[] args) {
+			SpringApplication.run(DevopsAssignmentApplication.class, args);
+		}
+
+		@Override
+		public void run(String... args) throws Exception {
+			System.out.println("Hello Dev-Ops");
+			
+		}
+
 	}
-
-}
